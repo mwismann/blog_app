35d81d4c77b592abc8df758b7033614726201f9d
mwismann <mwbertolucci@gmail.com>
2023-09-28 18:35:33 -0400

    Add Post methods to update posts_counter and to get last comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,15 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  after_create :update_posts_count
+  after_destroy :update_posts_count
+
+  def update_posts_count
+    author.update(posts_counter: author.posts.count)
+  end
+
+  def last_five_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
