9e6224d74d0a0c201ac18ba05ae5ef8f763fc322
mwismann <mwbertolucci@gmail.com>
2023-09-29 15:31:45 -0400

    Add Post model validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,10 @@ class Post < ApplicationRecord
   after_create :update_posts_counter
   after_destroy :update_posts_counter
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+
   def update_posts_counter
     author.update(posts_counter: author.posts.count)
   end
